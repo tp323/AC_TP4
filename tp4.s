05a5bcedc98cdf334bcd745ec63ecebacc16fca1
manuel-48052 <80792945+manuel-48052@users.noreply.github.com>
2022-06-15 19:55:35 +0100

    aula inicio
    aula working

--- tp4.s
+++ tp4.s
@@ -91,25 +91,12 @@ SYS_init:
 	ldr r1, ball_pos_addr_ddd
 	strb r0, [r1]
 	
-<<<<<<< Updated upstream
-/* Constante relacioandas com a aplicacao. */	
-	.equ	IE_FLAG, 			0x10
-	.equ	OUTPORT_INITVALUE,	0x00
-	.equ	LED_ON, 			1
-	.equ	LED_OFF, 			0
-	.equ	FIELD_MASK,			0xFE
-	.equ	PLAYER_MASK,		0x80
-	.equ	WALL_MASK,			0x02
-	.equ	RAKET_MASK,			0x01
-	.equ	LEVEL_MASK			0xC0
-=======
 	
 	mov r0, #1
 	mov r1, 0xE0
 	bl timer_write		
 	;bl timer_clearInterrupt	
 
->>>>>>> Stashed changes
 
 
 	mov r0, IE_MASK
@@ -124,125 +111,6 @@ ticks_addrb:
 	.word ticks
 
 main:
-<<<<<<< Updated upstream
-	push lr
-	push r4 
-	push r5
-	push r6
-
-init_game:	
-	bl outport_init
-	/* Inicializa o porto de saida e os valores necessarios ao inicio do jogo: posição, pontos e direção.
-	score set score = 0*/
-	mov r4, PLAYER_MASK // Ball current index0 
-	mov r0, PLAYER_MASK	
-	bl LED_set_on
-
-	mov r5, #1			// Direction (1- UP, 0 - DOWN)
-	mov r0, #0	
-	bl set_score 		//score set score = 0
-
-	mov r0, #50
-	bl timer_init
-	bl IRQ_enable
-	
-wait_for_init_stroke:	
-	bl get_level
-	mov r0, RAKET_MASK
-	bl check_swing
-	sub r0,r0,0
-	bzs wait_for_init_stroke
-	
-start_game:
-	
-	bl init_countdowns
-	bl move_ball
-	
-loop_game:
-	bl count_point_check
-	bl count_down_level_check
-
-	bl is_player
-	mov r1, #0
-	cmp r0, r1
-	beq loop_game
-
-	bl check_swing
-	cmp r0, r1
-	beq loop_game
-
-
-	mov r0, r4
-	bl move_ball
-	mov r4, r0
-	bl check_position
-	
-	b loop_game
-
-game_over:
-	bl timer_stop
-	pop r6
-	pop r5
-	pop r4
-	pop pc
-
-
-/* --------------------------------------------------------------------- AUXILIARY FUNCTIONS --------------------------------------------------------------------- */
-
-get_level:
-	push lr
-	bl port_read
-	mov r1, LEVEL_MASK
-	and r0, r0, r1
-	mov r1, #6
-	lsr r0, r0, r1
-	ldr r1, level_speed_addr
-	ldr r0, [r1, r0]	// Level speed stored in r0
-	ldr r1 current_speed_addr
-	strb r0, [r1]
-	pop pc
-
-level_speed_addr:
-	.level_speed
-
-current_speed_addr:
-	.current_speed
-
-	.equ 	LED_NEW_POINT,		0x01
-	//Assumindo 50ms (Utilizar clock de 1kHZ)
-	.equ	POINT_TIME, 			20
-	.equ	POINT_LED_TIME,		5
-	.equ	LEVEL,				16 //TEMP
-
-/*
-	The point countdown will start with a the number of ticks required to score a point (1 second) plus the time
-	for the LED to turn off. For each tick the variable will be decremented by one, once one second passes, a point
-	will be scored and the LED turns on. The remaining time in the variable will correspond to the number of ticks
-	the LED must remain active. Once it reaches zero, the LED turns off and the cycle restarts.
-*/
-
-//Initiates the countdown timer with the value passed as parameter.
-//void init_timer_point(int value)
-init_countdown_point:
-	ldr r1, countdown_point_adrr
-	str r0, [r1]
-	mov pc, lr
-
-//Decrements the variable by one
-//void countdown_point_decrement()
-countdown_point_decrement:
-	mov r0, #1
-	ldr r1, countdown_point_adrr
-	ldr r2, [r1]
-	sub r2, r2, r0
-	str r2, [r1]
-	mov pc, lr
-
-//Checks the time of the countdown timer. If the remaining time is 250ms, activates de LED and scores a point. If it reaches
-//zero turns off the LED and resets
-//void count_point_check()
-countdown_point_check:
-=======
 	push lr	
 	bl timer_stop
 	ldr r0, direction_addr_cc
@@ -399,7 +267,6 @@ ball_pos_addrb:
 	.word 	ball_pos	
 ; set led new point to the valu of r0	
 set_led_newpoint:
->>>>>>> Stashed changes
 	push lr
 	ldr r1, new_point_led_addrbbb
 	strb r0, [r1]
@@ -430,99 +297,18 @@ init_timer_lvl:
 	
 	
 
-<<<<<<< Updated upstream
-countdown_point_adrr:
-	.word countdown_point
-
-// ----------------------------------------------------------------
-/*
-	The level countdown will start with a the number of ticks required for each ball movement. Each tick will
-	countdown the variable and once it reaches zero, the ball moves and the cycle restarts.
-*/
-	
-//Initiates the countdown timer with the value passed as parameter.
-//void init_timer_level(int value)
-init_countdown_level:
-	ldr r1, countdown_level_adrr
-	str r0, [r1]
-	mov pc, lr
-
-//Decrements the variable by one
-//void countdown_point_decrement()
-countdown_level_decrement:
-	mov r0, #1
-	ldr r1, countdown_level_adrr
-	ldr r2, [r1]
-	sub r2, r2, r0
-	str r2, [r1]
-	mov pc, lr
-
-//Checks if the countdown reached 0 and if it did, calls level_reached
-//void count_down_level_check()
-count_down_level_check:
-	push lr
-	ldr r1, countdown_level_adrr
-	ldr r2, [r1]
-	mov r0, #0
-	cmp r0, r2
-	beq level_reached
-	pop pc
-=======
 
 timer_level_adrrb:
 	.word timer_level
->>>>>>> Stashed changes
 
 
 
 
-<<<<<<< Updated upstream
-// ----------------------------------------------------------------		
-
-init_countdowns:
-	push lr
-	mov r0, POINT_TIME
-	bl init_countdown_point
-	ldr r0, level_speed_addr
-	ldr r0, [r0]
-	bl init_countdown_level
-	pop pc
-
-//Updates the score variable in memory with the score passed as parameter
-// void set_score(int score)
-set_score:
-	push lr
-	ldr r1, score_addr
-	str r0, [r1]
-	pop pc
-=======
->>>>>>> Stashed changes
 	
 
 
 
 
-<<<<<<< Updated upstream
-//Checks if the position the ball is currently in is either the wall or the player
-//void check_position(positionIndex)
-check_position:
-	push lr
-	push r4
-
-	mov r4, r0
-	bl is_wall
-	mov r1, #1
-	cmp r0, r1
-	beq	invert_direction
-
-	mov r0, r4
-	bl is_player
-	mov r1, #1
-	cmp r0, r1
-	beq invert_direction
-
-	pop r4
-=======
 ball_pos_addr:
 	.word 	ball_pos
 	
@@ -566,7 +352,6 @@ finish_mov:
 	ldr r1, ball_pos_addr_cc
 	strb r0, [r1]
 	
->>>>>>> Stashed changes
 	pop pc
 	
 	
@@ -576,52 +361,6 @@ direction_addr_bb:
 ball_pos_addr_cc:
 		.word ball_pos
 
-<<<<<<< Updated upstream
-//Check if the current position it the wall
-//boolean is_player(currentIndex)
-is_player:
-	mov r1, PLAYER_MASK
-	cmp r0, r1
-	beq player_found
-	mov r0, #0
-	mov pc lr
-
-player_found:
-	mov r0, #1
-	mov pc, lr
-
-
-//Check if the current position it the player
-//boolean is_wall(currentIndex)
-is_wall:
-	mov r1, WALL_MASK
-	cmp r0, r1
-	beq wall_found
-	mov r0, #0
-	mov pc lr
-
-wall_found:
-	mov r0, #1
-	mov pc, lr
-
-
-//Switches direction
-//void invert_direction()
-invert_direction:
-	mov r0, #1
-	eor r5, r5, r0
-	mov pc lr
-	
-	
-/* Acende o led no índice idx do porto de saída.
-void LED_set_on(uint8_t idx);
-*/
-LED_set_on:
-	push	lr
-	mov		r1, LED_ON
-	bl		port_write_bit
-	pop		pc
-=======
 ;-------------------------------------------------------------------------
 ; Rotina:    isr
 ; Descricao: Rotina responsavel pelo processamento do pedido de interrupcao.
@@ -654,7 +393,6 @@ isr:
 	pop		r0
 	movs	pc, lr
 	
->>>>>>> Stashed changes
 
 	
 timer_clearInterrupt:
@@ -744,35 +482,6 @@ timer_init:
 ptc_addr:
 	.word pTC_ADDRESS
 
-<<<<<<< Updated upstream
-
-//------------------------------------------------ SUBSTITUIR PELA FORNECIDA NAS AULAS
-//Checks if there was a transiction from 0 to 1
-//boolean check_swing()
-check_swing:
-	push lr
-	mov r0, sw_state_address
-	ldr r0, [r0]
-	mov r1, #0
-	cmp r0, r1	//checking if the last recorded position is racket down
-	bne no_swing
-
-	mov r1, 0x0F
-	mov r2, #0
-	bl port_read_bit
-	mov r1, #0
-	cmp r0, r1
-	bne swing
-
-no_swing:
-	mov r0 #0
-
-swing:
-	mov r1, sw_state_address
-	strb r0, [r1]
-	pop pc
-//------------------------------------------------
-=======
 ;---------------------------------------------------------------------------------	
 ;uint8_t sw_is_pressed(uint8_t pin_mask) {
 ;uint8_t sw_new_state;
@@ -809,20 +518,10 @@ sw_is_pressed_0:
 	mov		r0, #0
 sw_is_pressed_1:
 	pop		pc
->>>>>>> Stashed changes
 
 sw_state_address:
 	.word	sw_state
 
-<<<<<<< Updated upstream
-/* Interrupt Service Routine */
-isr:
-	push lr
-	bl countdown_point_decrement
-	bl countdown_level_decrement
-	pop lr
-	movs	pc, lr; PC = LR; CPSR = SPSR
-=======
 ;---------------------------------------------------------------------------------	
 ;uint16_t inport_test_bits(uint16_t pins_mask) {
 ;	return ((inport_read() & pins_mask) == pins_mask);
@@ -850,7 +549,6 @@ end_inport_test_bit_1:
 end_inport_test_bit:
 	pop		r4
 	pop		pc
->>>>>>> Stashed changes
 	
 ;---------------------------------------------------------------------------------	
 ;uint16_t inport_read() {
@@ -1009,16 +707,6 @@ current_lvl_dificult_in_time:
 	
 score:
 	.word	0
-<<<<<<< Updated upstream
-current_speed:
-	.word 0
-level_speed:
-	.byte 12, 6, 3
-racket_position:
-	.byte 0
-port_img:
-	.space	1
-=======
 	
 ticks:
 	.word	0		; uint16_t ticks;
@@ -1031,7 +719,6 @@ new_point_led:
 	
 direction: ; 0 away from player 1 into the player
 	.byte	0x00
->>>>>>> Stashed changes
 sw_state:
 	.byte 	0
 ; Seccao:    .bss
